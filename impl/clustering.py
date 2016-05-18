62f3f452bfda519a4ae5bb03dee4e01745da8465
Marten Lienen <marten.lienen@gmail.com>
2016-05-18 14:33:41 +0200

    Make clusters configurable

--- impl/clustering.py
+++ impl/clustering.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import collections
 import operator as op
 
 import numpy as np
@@ -9,17 +10,75 @@ import cvxopt as cvx
 import matplotlib.pyplot as pp
 import networkx as nx
 import picos as pic
+import scipy
 import scipy.spatial
+import scipy.stats
+
+
+# Stochastic ball model cluster
+class BallCluster:
+    def __init__(self, n, p, r):
+        self.n = int(n)
+        self.p = np.array(p)
+        self.r = float(r)
+
+    # Sample points in the r-ball with rejection sampling
+    def samples(self):
+        m = len(self.p)
+        P = np.zeros((m, self.n))
+        for i in range(self.n):
+            p = scipy.stats.uniform.rvs(loc=-self.r, scale=2 * self.r, size=m)
+
+            while scipy.linalg.norm(p) > self.r:
+                p = scipy.stats.uniform.rvs(size=m)
+
+            P[:, i] = p + self.p
+
+        return P
+
+
+# Normally distributed cluster
+class NormalCluster:
+    def __init__(self, n, mu, sigma):
+        self.n = int(n)
+        self.mu = np.array(mu)
+        self.sigma = np.array(sigma)
+
+    def samples(self):
+        return scipy.stats.multivariate_normal.rvs(mean=self.mu,
+                                                   cov=self.sigma,
+                                                   size=self.n).T
+
+
+def parsecluster(string):
+    return eval(string, {"__builtins__": None}, {"Ball": BallCluster,
+                                                 "Normal": NormalCluster})
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--cluster",
+    default=[BallCluster(15, [-2, -1], 1), BallCluster(15, [3, 1], 1.5)],
+    type=parsecluster,
+    nargs="*",
+    help="Cluster specifications")
+parser.add_argument("-e",
+                    "--epsilon",
+                    default=None,
+                    type=float,
+                    help="Epsilon parameter")
+
+args = parser.parse_args()
+
+### Setup ###
 
 # Generate points
-n = 15
-k = 2
-d_min = np.sqrt(18)
-P = []
-P.append(np.random.normal(-1, 1, (2, n)))
-P.append(np.random.normal(3, 2, (2, n)))
-P = np.hstack(P)
+clusters = args.cluster
+k = len(clusters)
+P = np.hstack([c.samples() for c in clusters])
 N = P.shape[1]
+
+### Clustering ###
 
 # Compute pairwise distances
 D = scipy.spatial.distance.pdist(P.T)
@@ -38,7 +97,42 @@ sdp.minimize(Dparam | X)
 # Extract the SDP solution
 X_D = np.array(X.value)
 
-# Plot original data colored by cluster
+# Denoised data
+PX_D = P @X_D
+
+# Rounding
+if args.epsilon:
+    epsilon = args.epsilon
+else:
+    epsilon = 1.5
+
+# Construct "adjacency" graph
+G = nx.Graph()
+G.add_nodes_from(range(N))
+for i in range(N):
+    for j in range(i):
+        if D[i, j] <= 2 * epsilon:
+            G.add_edge(i, j)
+
+# Extract nodes of maximum degree as cluster centers
+centers = []
+for i in range(k):
+    if G.size() == 0:
+        print("Epsilon {} is too large. Graph is empty in round {}.".format(
+            epsilon, i + 1))
+        exit()
+
+    center = sorted(G.degree_iter(), key=op.itemgetter(1), reverse=True)[0][0]
+
+    for j in G.nodes():
+        if D[center, j] <= 4 * epsilon:
+            G.remove_node(j)
+
+    centers.append(P[:, center])
+
+### Plotting ###
+
+# Plot original and denoised data colored by cluster
 COLORS = ["r", "g", "b"]
 nodes = set(range(N))
 clusternr = 0
@@ -52,40 +146,31 @@ while len(nodes) > 0:
             cluster.append(i)
             nodes.remove(i)
 
-    points = P[:, cluster]
-    pp.plot(points[0, :],
-            points[1, :],
+    orig = P[:, cluster]
+    denoised = PX_D[:, cluster]
+
+    pp.plot(orig[0, :],
+            orig[1, :],
             "o",
             color=COLORS[clusternr],
             label="Cluster {}".format(clusternr + 1))
 
+    pp.plot(denoised[0, :],
+            denoised[1, :],
+            "x",
+            ls="",
+            color=COLORS[clusternr],
+            label="Denoised Data {}".format(clusternr + 1))
+
     clusternr += 1
 
-# Plot denoised data
-PX_D = P @X_D
-pp.plot(PX_D[0, :], PX_D[1, :], "x", ls="", label="Denoised Data")
+# Plot cluster centers
+for i, center in enumerate(centers):
+    pp.plot([center[0]],
+            [center[1]],
+            "*",
+            label="Center {}".format(i + 1),
+            ms=15)
 
-# Rounding
-epsilon = d_min / 9
-
-# Construct "adjacency" graph
-G = nx.Graph()
-G.add_nodes_from(range(N))
-for i in range(N):
-    for j in range(i):
-        if D[i, j] <= 2 * epsilon:
-            G.add_edge(i, j)
-
-# Extract nodes of maximum degree as cluster centers
-for i in range(k):
-    center = sorted(G.degree_iter(), key=op.itemgetter(1), reverse=True)[0][0]
-
-    for j in G.nodes():
-        if D[center, j] <= 4 * epsilon:
-            G.remove_node(j)
-
-    point = P[:, center]
-    pp.plot([point[0]], [point[1]], "*", label="Center {}".format(i))
-
-pp.legend(loc="best", framealpha=0.5)
+pp.legend(loc="best", framealpha=0.5, numpoints=1)
 pp.show()
