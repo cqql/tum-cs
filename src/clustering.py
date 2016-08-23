226b01da072028cc94b320db3a2741a806c786bc
Marten Lienen <marten.lienen@gmail.com>
2016-08-23 12:04:18 -0400

    Split the implementation

--- src/clustering.py
+++ src/clustering.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
-import collections
-import operator as op
 
-import numpy as np
-
-import cvxopt as cvx
 import matplotlib.pyplot as pp
-import networkx as nx
-import picos as pic
+import numpy as np
 import scipy
-import scipy.spatial
 import scipy.stats
+import sdpclstr
 
 
 # Stochastic ball model cluster
@@ -61,116 +55,6 @@ def parsecluster(string):
                                                  "Normal": NormalCluster})
 
 
-def sdpcluster(k, P, epsilon=None):
-    """Run the SDP clustering algorithm.
-
-    Parameters
-    ----------
-    k : int
-        Number of clusters
-    P : np.array
-        m*N matrix of N m-dimensional points stacked columnwise.
-    epsilon : float, optional
-        Radius of the ball used for medoid determination in the end.
-
-        If no value is given, it will be estimated from the denoised data.
-
-    Returns
-    -------
-    (PX_D, clusters, centers)
-    PX_D : np.array
-        Denoised points
-    clusters : list(list(int))
-        Lists of indices of points that probably belong to the same cluster
-    centers : np.array
-        m*k matrix of k cluster-medoids
-    """
-    N = P.shape[1]
-
-    # Compute pairwise distances
-    D = scipy.spatial.distance.pdist(P.T)
-    D = scipy.spatial.distance.squareform(D)
-
-    # Create the semi-definite program
-    sdp = pic.Problem()
-    Dparam = pic.new_param("D", D)
-    X = sdp.add_variable("X", (N, N), vtype="symmetric")
-    sdp.add_constraint(pic.trace(X) == k)
-    sdp.add_constraint(X * "|1|({},1)".format(N) == 1)
-    sdp.add_constraint(X > 0)
-    sdp.add_constraint(X >> 0)
-    sdp.minimize(Dparam | X)
-
-    # Extract the SDP solution
-    X_D = np.array(X.value)
-
-    # Denoised data
-    PX_D = P @ X_D # yapf: disable
-
-    # Extract approximate cluster affinities
-    nodes = set(range(N))
-    clusters = []
-    while len(nodes) > 0:
-        node = nodes.pop()
-        row = X_D[node, :]
-
-        cluster = set([node])
-        for i in range(N):
-            if i in nodes and row[i] > 1 / N:
-                cluster.add(i)
-                nodes.remove(i)
-
-        clusters.append(cluster)
-
-    # Rounding
-    if not epsilon:
-        # Compute cluster means from denoised data
-        means = [np.mean(PX_D[:, list(c)], axis=1) for c in clusters]
-
-        # Compute minimum distance as approximation to d_min
-        d_min = min(scipy.spatial.distance.pdist(means))
-
-        # Use something slightly smaller than the upper bound d_min / 8
-        epsilon = d_min / 10
-
-        print("Using epsilon = {:.3f}".format(epsilon))
-
-    # Compute pairwise distances of denoised data
-    D = scipy.spatial.distance.pdist(PX_D.T)
-    D = scipy.spatial.distance.squareform(D)
-
-    # Construct "adjacency" graph from denoised distances
-    G = nx.Graph()
-    G.add_nodes_from(range(N))
-    for i in range(N):
-        for j in range(i):
-            if D[i, j] <= 2 * epsilon:
-                G.add_edge(i, j)
-
-    # Extract nodes of maximum degree as cluster centers
-    centers = []
-    for i in range(k):
-        if G.size() == 0:
-            print(
-                "Epsilon {} is too large. Graph is empty in round {}.".format(
-                    epsilon, i + 1))
-            exit()
-
-        center = sorted(G.degree_iter(),
-                        key=op.itemgetter(1),
-                        reverse=True)[0][0]
-
-        for j in G.nodes():
-            if D[center, j] <= 4 * epsilon:
-                G.remove_node(j)
-
-        centers.append(center)
-
-    centers = PX_D[:, centers]
-
-    return (PX_D, clusters, centers)
-
-
 def main():
     # Parse arguments
     parser = argparse.ArgumentParser()
@@ -216,7 +100,7 @@ def main():
         offset += S.shape[1]
 
     # Clustering
-    PX_D, clusters, centers = sdpcluster(k, P, args.epsilon)
+    PX_D, clusters, centers = sdpclstr.cluster(k, P, args.epsilon)
 
     ### Plotting ###
 
