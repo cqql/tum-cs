238389b681d989635f628802f85089913459a83f
Marten Lienen <marten.lienen@gmail.com>
2016-08-25 12:59:32 -0400

    Make the input parsing reusable

--- src/clustering.py
+++ src/clustering.py
@@ -2,57 +2,11 @@
 
 import argparse
 
+import data
 import matplotlib.pyplot as pp
 import numpy as np
 import scipy
-import scipy.stats
 import sdpclstr
-
-
-# Stochastic ball model cluster
-class BallCluster:
-    def __init__(self, n, p, r):
-        self.n = int(n)
-        self.p = np.array(p)
-        self.r = float(r)
-
-    # Sample points in the r-ball with rejection sampling
-    def samples(self):
-        m = len(self.p)
-        P = np.zeros((m, self.n))
-        for i in range(self.n):
-            p = scipy.stats.uniform.rvs(loc=-self.r, scale=2 * self.r, size=m)
-
-            while scipy.linalg.norm(p) > self.r:
-                p = scipy.stats.uniform.rvs(size=m)
-
-            P[:, i] = p + self.p
-
-        return P
-
-    def center(self):
-        return self.p
-
-
-# Normally distributed cluster
-class NormalCluster:
-    def __init__(self, n, mu, sigma):
-        self.n = int(n)
-        self.mu = np.array(mu)
-        self.sigma = np.array(sigma)
-
-    def samples(self):
-        return scipy.stats.multivariate_normal.rvs(mean=self.mu,
-                                                   cov=self.sigma,
-                                                   size=self.n).T
-
-    def center(self):
-        return self.mu
-
-
-def parsecluster(string):
-    return eval(string, {"__builtins__": None}, {"Ball": BallCluster,
-                                                 "Normal": NormalCluster})
 
 
 def main():
@@ -60,15 +14,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--cluster",
-        default=[BallCluster(15, [-2, -1], 1), BallCluster(15, [3, 1], 1.5)],
-        type=parsecluster,
+        default=[data.BallCluster(15, [-2, -1], 1),
+                 data.BallCluster(15, [3, 1], 1.5)],
+        type=data.fromfile,
         nargs="*",
         help="Cluster specifications")
-    parser.add_argument("-e",
-                        "--epsilon",
-                        default=None,
-                        type=float,
-                        help="Epsilon parameter")
+    parser.add_argument(
+        "-e", "--epsilon", default=None, type=float, help="Epsilon parameter")
     parser.add_argument("--seed", default=10, type=int, help="Random seed")
 
     args = parser.parse_args()
@@ -148,54 +100,52 @@ def main():
         orig = P[:, list(cluster)]
         denoised = PX_D[:, list(cluster)]
 
-        pp.plot(orig[0, :],
-                orig[1, :],
-                "o",
-                color=COLORS[i],
-                label="Cluster {}".format(i + 1))
+        pp.plot(
+            orig[0, :],
+            orig[1, :],
+            "o",
+            color=COLORS[i],
+            label="Cluster {}".format(i + 1))
 
-        pp.plot(denoised[0, :],
-                denoised[1, :],
-                "d",
-                ls="",
-                ms=12,
-                color=COLORS[i],
-                label="Denoised Data {}".format(i + 1))
+        pp.plot(
+            denoised[0, :],
+            denoised[1, :],
+            "d",
+            ls="",
+            ms=12,
+            color=COLORS[i],
+            label="Denoised Data {}".format(i + 1))
 
         # Plot k-means-optimal centroids
         centroid = optimalcentroids[min(len(models) - 1, i)]
-        pp.plot(centroid[0],
-                centroid[1],
-                "h",
-                ms=12,
-                ls="",
-                color=COLORS[i],
-                label="Optimal Centroid {}".format(i + 1))
+        pp.plot(
+            centroid[0],
+            centroid[1],
+            "h",
+            ms=12,
+            ls="",
+            color=COLORS[i],
+            label="Optimal Centroid {}".format(i + 1))
 
         # Plot true cluster centers
         center = truecenters[min(len(models) - 1, i)]
-        pp.plot(center[0],
-                center[1],
-                "^",
-                ms=15,
-                ls="",
-                color=COLORS[i],
-                label="True Center {}".format(i + 1))
+        pp.plot(
+            center[0],
+            center[1],
+            "^",
+            ms=15,
+            ls="",
+            color=COLORS[i],
+            label="True Center {}".format(i + 1))
 
     # Mark misclassified points
     Pmisclass = P[:, misclass]
-    pp.plot(Pmisclass[0, :],
-            Pmisclass[1, :],
-            "xr",
-            ms=12,
-            label="Misclassified")
+    pp.plot(
+        Pmisclass[0, :], Pmisclass[1, :], "xr", ms=12, label="Misclassified")
 
     # Plot cluster center approximators
-    pp.plot(centers[0, :],
-            centers[1, :],
-            "*y",
-            label="Approximate centers",
-            ms=12)
+    pp.plot(
+        centers[0, :], centers[1, :], "*y", label="Approximate centers", ms=12)
 
     pp.legend(loc="best", framealpha=0.5, numpoints=1)
     pp.show()
