f7f288fd050964c020cbe940dda499d260822798
Marten Lienen <marten.lienen@gmail.com>
2016-08-28 18:08:22 -0400

    Describe experimental results

--- src/clustering.py
+++ src/clustering.py
@@ -12,18 +12,20 @@ import sdpclstr
 def main():
     # Parse arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument("--out", help="File to plot into")
     parser.add_argument(
         "--cluster",
         default=[data.BallCluster(15, [-2, -1], 1),
                  data.BallCluster(15, [3, 1], 1.5)],
         type=data.fromfile,
-        nargs="*",
         help="Cluster specifications")
     parser.add_argument(
         "-e", "--epsilon", default=None, type=float, help="Epsilon parameter")
     parser.add_argument("--seed", default=10, type=int, help="Random seed")
 
     args = parser.parse_args()
+    epsilon = args.epsilon
+    outfile = args.out
 
     # Seed random state
     np.random.seed(args.seed)
@@ -36,10 +38,10 @@ def main():
     m, N = P.shape
 
     # Save true cluster centers
-    truecenters = [m.center() for m in models]
+    truecenters = np.array([m.center() for m in models]).T
 
     # Save the k-means-optimal centroids
-    optimalcentroids = [np.mean(S, axis=1) for S in samples]
+    optimalcentroids = np.array([np.mean(S, axis=1) for S in samples]).T
 
     # Save true cluster affinities
     offset = 0
@@ -52,7 +54,9 @@ def main():
         offset += S.shape[1]
 
     # Clustering
-    PX_D, clusters, centers = sdpclstr.cluster(k, P, args.epsilon)
+    PX_D, clusters, centers = sdpclstr.cluster(k, P, epsilon)
+    # PX_D, clusters, centers = sdpclstr.cluster(k, PX_D, epsilon)
+    # PX_D, clusters, centers = sdpclstr.cluster(k, PX_D, epsilon)
 
     ### Plotting ###
 
@@ -78,8 +82,8 @@ def main():
     optimalerror = 0
     tmp = set(range(centers.shape[1]))
     for i in range(k):
-        tcenter = truecenters[i]
-        optimal = optimalcentroids[i]
+        tcenter = truecenters[:, i]
+        optimal = optimalcentroids[:, i]
         match = min(tmp,
                     key=lambda c: scipy.linalg.norm(centers[:, c] - tcenter))
 
@@ -93,6 +97,77 @@ def main():
 
     if m != 2:
         return
+
+    # Compute axis x and y limits
+    xmin, xmax = P[0, :].min(), P[0, :].max()
+    ymin, ymax = P[1, :].min(), P[1, :].max()
+    xrange, yrange = xmax - xmin, ymax - ymin
+    xpad, ypad = 0.05 * xrange, 0.05 * yrange
+
+    fig, axes = pp.subplots(nrows=1, ncols=3)
+    ax1, ax2, ax3 = axes
+    fig.set_size_inches(12, 4)
+
+    for ax in axes:
+        ax.set_xlim(xmin - xpad, xmax + xpad)
+        ax.set_ylim(ymin - ypad, ymax + ypad)
+
+    # Plot raw data
+    ax1.plot(P[0, :], P[1, :], "ow", label=r"$x_{a,i}$")
+    ax1.plot(
+        truecenters[0, :],
+        truecenters[1, :],
+        "xk",
+        ms=15,
+        label=r"$\gamma_{a}$")
+
+    # Plot denoised data
+    ax2.plot(PX_D[0, :], PX_D[1, :], "ow", label=r"$c_{a,i}$")
+    ax2.plot(
+        truecenters[0, :],
+        truecenters[1, :],
+        "xk",
+        ms=15,
+        label=r"$\gamma_{a}$")
+
+    # Plot gamma-hat, gamma-tilde
+    ax3.plot(
+        centers[0, :],
+        centers[1, :],
+        "*",
+        mec="k",
+        mfc=(1, 1, 1, 0),
+        label=r"$v_a$",
+        ms=15)
+    ax3.plot(
+        optimalcentroids[0, :],
+        optimalcentroids[1, :],
+        "^",
+        ms=12,
+        mec="k",
+        mfc=(1, 1, 1, 0),
+        ls="",
+        label=r"$\tilde{\gamma}_a$")
+    ax3.plot(
+        truecenters[0, :],
+        truecenters[1, :],
+        "xk",
+        ms=15,
+        label=r"$\gamma_{a}$")
+
+    fig.subplots_adjust(wspace=0.5)
+    ax1.legend(loc="upper right", framealpha=0.5, numpoints=1)
+    ax2.legend(loc="upper right", framealpha=0.5, numpoints=1)
+    ax3.legend(loc="upper right", framealpha=0.5, numpoints=1)
+
+    if outfile:
+        fig.savefig(outfile)
+
+    pp.show()
+
+    return
+
+    ###### Prev code
 
     # Plot original and denoised data colored by cluster
     COLORS = ["c", "m", "b", "g", "y"]
@@ -117,7 +192,7 @@ def main():
             label="Denoised Data {}".format(i + 1))
 
         # Plot k-means-optimal centroids
-        centroid = optimalcentroids[min(len(models) - 1, i)]
+        centroid = optimalcentroids[:, min(len(models) - 1, i)]
         pp.plot(
             centroid[0],
             centroid[1],
